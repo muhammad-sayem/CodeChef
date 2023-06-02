92b014e82c1a5a1ee1e809bceacfae21708d0ae0
muhammad-sayem <shahrulislamsayem@gmail.com>
2023-06-02 17:42:58 +0600

    TWOVSTEN (936)

--- Two_Vs_Ten.cpp
+++ Two_Vs_Ten.cpp
@@ -24,7 +24,7 @@ int main()
         int n;
         cin >> n;
 
-        if(n%10 == 0) cout <<0 << endl;
+        if(n%10 == 0) cout << 0 << endl;
         else if(n%5 == 0) cout << 1 << endl;
         else cout << -1 << endl;
     }
