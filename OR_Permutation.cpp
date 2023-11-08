2ff921d6258701b02ca136d448193a9e71fa6f8b
muhammad-sayem <shahrulislamsayem@gmail.com>
2023-11-08 23:19:05 +0600

    PERMOR

---
+++ OR_Permutation.cpp
@@ -0,0 +1,76 @@
+#include<bits/stdc++.h>
+#define      endl            '\n'
+#define      yes             (cout << "YES\n")
+#define      no              (cout << "NO\n")
+#define      ll              long long
+#define      pb              push_back
+#define      pii             pair <int,int>
+#define      pll             pair <long long,long long>
+#define      rev(a)          reverse(a, a+n);
+#define      Srev(s)         reverse(s.begin(), s.end());
+#define      sz(s)           s.size()
+#define      gcd(a,b)        __gcd(a,b)
+#define      lcm(a,b)        (a*b)/gcd(a,b)
+#define      fastIO          ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+int main()
+{
+    fastIO;
+
+    int test;
+    cin >> test;
+
+    while(test--){
+        int n, i;
+        cin >> n;
+
+        for(i=n; i>=1; i--){
+            cout << i << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
+// ----------------------------------------------------------------- //
+
+/*
+#include<bits/stdc++.h>
+#define      endl            '\n'
+#define      yes             (cout << "YES\n")
+#define      no              (cout << "NO\n")
+#define      ll              long long
+#define      pb              push_back
+#define      pii             pair <int,int>
+#define      pll             pair <long long,long long>
+#define      rev(a)          reverse(a, a+n);
+#define      Srev(s)         reverse(s.begin(), s.end());
+#define      sz(s)           s.size()
+#define      gcd(a,b)        __gcd(a,b)
+#define      lcm(a,b)        (a*b)/gcd(a,b)
+#define      fastIO          ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+int main()
+{
+    fastIO;
+
+    int test;
+    cin >> test;
+
+    while(test--){
+        ll n, i;
+        cin >> n;
+
+        cout << 1 << " ";
+
+        for(i=3; i<=n; i++){
+            if(i%2 != 0) cout << i << " " << i-1 << " ";
+        }
+        if(n%2 == 0) cout << n << endl;
+        else cout << endl;
+    }
+
+    return 0;
+}
+*/
